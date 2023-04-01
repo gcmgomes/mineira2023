8d5f381e5717d7b6db0b6c69a22733d28e274f01
Guilherme Gomes <gcmgomes>
2023-04-01 09:14:10 -0300

    problem elemento

---
+++ problems/elemento/solutions/good/gg.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> v(4);
+  for (auto& x : v) {
+    cin >> x;
+  }
+  if (v[1] == 1 && v[2] == 2 && v[3] == 3) cout << 1;
+  if (v[0] == 1 && v[2] == 1 && v[3] == 2) cout << 2;
+  if (v[0] == 2 && v[1] == 1 && v[3] == 1) cout << 3;
+  if (v[0] == 3 && v[1] == 2 && v[2] == 1) cout << 4;
+
+  cout << endl;
+  return 0;
+}
