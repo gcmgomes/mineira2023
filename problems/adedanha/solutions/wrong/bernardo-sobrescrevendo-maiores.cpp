c432d429fe1ae9fa20abdb29c5ed56cc5a19bc86
Bernardo Amorim <bernardotamorim@gmail.com>
2023-04-25 14:49:40 -0300

    Melhorando adedanha

--- problems/adedanha/solutions/wrong/bernardo-sobrescrevendo-maiores.cpp
+++ problems/adedanha/solutions/wrong/bernardo-sobrescrevendo-maiores.cpp
@@ -7,7 +7,7 @@ int main() { _
   int N, S = 0;
 
   std::cin >> N;
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N - 1; i++) {
     int dedos;
     std::cin >> dedos;
     S += dedos;
@@ -16,7 +16,7 @@ int main() { _
   std::vector<int> v(N, -1);
   for (int dedos_adriana = 0; dedos_adriana <= 20; dedos_adriana++)
     v[(S + dedos_adriana)%N] = dedos_adriana;
-
+    
   for (int dedos_necessarios : v) 
     std::cout << dedos_necessarios << ' ';
   std::cout << std::endl;
