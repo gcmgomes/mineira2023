6c85dad80b165911315b6daf588624493c1021b3
Bruno Monteiro <malettabruno@gmail.com>
2023-04-25 19:05:23 -0300

    pastel: fix slow solution

--- problems/pastel/solutions/slow/bruno.cpp
+++ problems/pastel/solutions/slow/bruno.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -22,78 +20,50 @@ vector<vector<int>> le() {
 	return ret;
 }
 
-vector<vector<int>> trans(vector<vector<int>> v) {
+vector<vector<int>> grundy(vector<vector<int>> v) {
 	int n = v.size(), m = v[0].size();
-	vector ret(m, vector(n, int()));
-	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) ret[j][i] = v[i][j];
-	return ret;
-}
-
-vector<vector<int>> get_grundy(vector<vector<int>> v) {
-	int n = v.size(), m = v[0].size();
-	vector<set<int>> baixo(m);
-	set<int> all;
-	for (int i = 0; i <= n+m; i++) all.insert(i);
-	vector<int> tirei;
 
 	for (int i = n-1; i >= 0; i--) {
 		for (int j = m-1; j >= 0; j--) {
 			if (v[i][j] == -1) {
-				baixo[j].clear();
-				for (int k : tirei) all.insert(k);
-				tirei.clear();
 				continue;
 			}
-			auto it = all.begin();
-			for (int k : baixo[j]) if (*it == k) it++;
-			int mex = *it;
+			vector<int> qt(n+m);
+			for (int ii = i+1; ii < n; ii++) {
+				if (v[ii][j] == -1) break;
+				qt[v[ii][j]] = 1;
+			}
+			for (int jj = j+1; jj < m; jj++) {
+				if (v[i][jj] == -1) break;
+				qt[v[i][jj]] = 1;
+			}
+			int mex = 0;
+			while (qt[mex]) mex++;
 			v[i][j] = mex;
-			all.erase(mex);
-			tirei.push_back(mex);
-			baixo[j].insert(mex);
 		}
-		for (int k : tirei) all.insert(k);
-		tirei.clear();
 	}
 	return v;
 }
 
-vector<vector<int>> grundy(vector<vector<int>> v) {
-	if (v.size() > v[0].size()) return trans(get_grundy(trans(v)));
-	return get_grundy(v);
-}
-
-vector<ll> mov(vector<vector<int>> v) {
+vector<int> mov(vector<vector<int>> v) {
 	v = grundy(v);
 	int n = v.size(), m = v[0].size();
-	vector<ll> ret(n+m);
+	vector<int> ret(n+m);
 	for (int i = 0; i < n; i++) if (v[i][0] >= 0) ret[v[i][0]]++;
 	for (int j = 1; j < m; j++) if (v[0][j] >= 0) ret[v[0][j]]++;
 	return ret;
 }
 
-template<char op, class T> vector<T> FWHT(vector<T> f, bool inv = false) {
-	int n = f.size();
-	for (int k = 0; (n-1)>>k; k++) for (int i = 0; i < n; i++) if (i>>k&1) {
-		int j = i^(1<<k);
-		if (op == '^') f[j] += f[i], f[i] = f[j] - 2*f[i];
-		if (op == '|') f[i] += (inv ? -1 : 1) * f[j];
-		if (op == '&') f[j] += (inv ? -1 : 1) * f[i];
-	}
-	if (op == '^' and inv) for (auto& i : f) i /= n;
-	return f;
-}
-
-vector<ll> conv(vector<ll> a, vector<ll> b) {
-	int N = max(a.size(), b.size());
-	while (N&(N-1)) N++;
-	a.resize(N), b.resize(N);
-	a = FWHT<'^'>(a), b = FWHT<'^'>(b);
-	for (int i = 0; i < a.size(); i++) a[i] = a[i] * b[i];
-	return FWHT<'^'>(a, true);
+ll conv(vector<int> a, vector<int> b, vector<int> c) {
+	ll ans = 0;
+	for (int i = 0; i < a.size(); i++)
+		for (int j = 0; j < b.size(); j++)
+			for (int k = 0; k < c.size(); k++)
+				if ((i^j^k) == 0) ans += (ll) a[i] * b[j] * c[k];
+	return ans;
 }
 
 int main() { _
-	cout << conv(mov(le()), conv(mov(le()), mov(le())))[0] << endl;
+	cout << conv(mov(le()), mov(le()), mov(le())) << endl;
 	exit(0);
 }
