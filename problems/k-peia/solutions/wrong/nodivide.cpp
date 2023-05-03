4be24b5dfed70024a6d127364d1f8c5ed85f74f1
Bruno Monteiro <malettabruno@gmail.com>
2023-05-03 20:28:44 -0300

    kpeia: add solucoes

---
+++ problems/k-peia/solutions/wrong/nodivide.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+	int n, T; cin >> n >> T;
+	vector<tuple<vector<int>, vector<int>, int>> moch(T); // {valor, peso, cap}
+	for (int i = 0; i < n; i++) {
+		int k, t, m; cin >> k >> t >> m;
+		get<0>(moch[t-1]).push_back(m);
+		get<1>(moch[t-1]).push_back(k);
+	}
+	for (int i = 0; i < T; i++) {
+		int l, r; cin >> l >> r;
+		get<2>(moch[i]) = min(l, r);
+	}
+	ll ans = 0;
+	for (auto& [v, p, c] : moch) {
+		vector<ll> dp(c+1);
+		for (int i = 0; i < v.size(); i++)
+			for (int j = c; j >= p[i]; j--) dp[j] = max(dp[j], v[i] + dp[j - p[i]]);
+		ans += dp[c];
+	}
+	cout << ans << endl;
+	exit(0);
+}
+
